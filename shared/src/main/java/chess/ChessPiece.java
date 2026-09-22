9a80d02304497499ec785f3db969aff187818f44
adam-clark-git <adam.b.clark@icloud.com>
2026-09-21 22:34:27 -0600

    Work in progress for ChessPiece functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -88,6 +88,28 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        PieceMoves moveSet = new PieceMoves(board, myPosition, getTeamColor());
+        switch (pieceType) {
+            case BISHOP:
+                return PieceMoves.getBishopMoves();
+                break;
+            case ROOK:
+                return PieceMoves.getRookMoves(board, myPosition);
+                break;
+            case KNIGHT:
+                return PieceMoves.getKnightMoves(board, myPosition);
+                break;
+            case QUEEN:
+                return PieceMoves.getQueenMoves(board, myPosition);
+                break;
+            case KING:
+                return PieceMoves.getKingMoves();
+                break;
+            case PAWN:
+                return PieceMoves.getPawnMoves();
+                break;
+            case null, default:
+                return null;
+        }
     }
 }
