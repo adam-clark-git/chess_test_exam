ddd3f5a243e3b4440271e3f2423be7c9b51a0b58
adam-clark-git <adam.b.clark@icloud.com>
2026-09-21 23:01:54 -0600

    PieceMoves should be working??????????

--- shared/src/main/java/chess/PieceMoves.java
+++ shared/src/main/java/chess/PieceMoves.java
@@ -4,40 +4,64 @@ import java.util.*;
 import java.util.function.BiFunction;
 
 public class PieceMoves {
-    private ChessBoard board;
-    private ChessPosition pos;
-    private ChessGame.TeamColor color;
+    final private ChessBoard board;
+    final private ChessPosition pos;
+    final private ChessGame.TeamColor color;
+    private List<ChessMove> moves;
 
     public PieceMoves(ChessBoard board, ChessPosition pos, ChessGame.TeamColor color) {
         this.board = board;
         this.pos = pos;
         this.color = color;
+        moves = new ArrayList<ChessMove>();
     }
     public enum CardDirections {
         NORTH,
+        NORTHEAST,
+        NORTHWEST,
         SOUTH,
+        SOUTHEAST,
+        SOUTHWEST,
         EAST,
         WEST
     }
-    public Collection<ChessMove> getBishopMoves() {
-        List<ChessMove> moves = new ArrayList<ChessMove>();
-        /*BiFunction<Integer, CardDirections, ChessPosition> movement = (num, dir) -> {
+    public Collection<ChessMove> getRookMoves() {
+        moves = new ArrayList<ChessMove>();
+        BiFunction<Integer, CardDirections, ChessPosition> movement = (num, dir) -> {
             switch(dir) {
-                case NORTH ->
+                case NORTH -> {return new ChessPosition(pos.getRow()+num, pos.getColumn());}
+                case SOUTH -> {return new ChessPosition(pos.getRow()-num, pos.getColumn());}
+                case EAST -> {return new ChessPosition(pos.getRow(), pos.getColumn()+num);}
+                case WEST -> {return new ChessPosition(pos.getRow(), pos.getColumn()-num);}
+                case null, default -> {return null;}
             }
-        }*/
-        return null;
+        };
+        straightMoves(movement);
+        return moves;
     }
-    private Collection<ChessMove> straightMoves(BiFunction<Integer, CardDirections, ChessPosition> movement) {
+    private void straightMoves(BiFunction<Integer, CardDirections, ChessPosition> movement) {
         for (CardDirections dir : CardDirections.values()) {
-
+            for (int i = 1; i < 8; i++) {
+                ChessPosition endPos = movement.apply(i,dir);
+                if (endPos == null) break;
+                ChessMove move = new ChessMove(pos,endPos, null);
+                if (!isValidMove(move)) {
+                    break;
+                }
+                moves.add(move);
+                if (isCapture(move)) {
+                    break;
+                }
+            }
         }
-        return null;
     }
-    private boolean isValidMove() {
-        return true;
+    private boolean isValidMove(ChessMove move) {
+        if (move.getEndPosition().getRow() < 1 || move.getEndPosition().getColumn() < 1 || move.getEndPosition().getRow() > 8 || move.getEndPosition().getColumn() > 8) {
+            return false;
+        }
+        return board.getPiece(move.getEndPosition()) == null || board.getPiece(move.getEndPosition()).getTeamColor() != color;
     }
-    private boolean isCapture() {
-        return true;
+    private boolean isCapture(ChessMove move) {
+        return board.getPiece(move.getEndPosition()) != null && board.getPiece(move.getEndPosition()).getTeamColor() != color;
     }
 }
