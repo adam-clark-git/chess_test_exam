d565c35b4e2592142ff57dd4779f2cc0c38d6998
adam-clark-git <adam.b.clark@icloud.com>
2026-09-21 21:36:30 -0600

    Basics on ChessPiece done.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -13,13 +13,15 @@ public class ChessPiece {
     private ChessGame.TeamColor pieceColor;
     private ChessPiece.PieceType pieceType;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.pieceType = type;
     }
     @Override
     public boolean equals(Object obj) {
         if (obj == this) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         ChessPiece other =  (ChessPiece) obj;
-        return other.getPieceType() != getPieceType() || other.getTeamColor() != getTeamColor();
+        return other.getPieceType() == getPieceType() && other.getTeamColor() == getTeamColor();
     }
 
     @Override
@@ -43,14 +45,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return pieceType;
     }
 
     /**
